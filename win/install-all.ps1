e10096b0a13f94630b88bbf1729cce92710f8548
Marta Karaniewicz-Biedna <marta.karaniewicz.biedna@gmail.com>
2023-12-18 18:10:59 +0100

    Fix

--- win/install-all.ps1
+++ win/install-all.ps1
@@ -1,5 +1,5 @@
 Write-Host "Installing system-wide components..." -ForegroundColor "Yellow"
-Start-Process -FilePath $PSScriptRoot\install-system.ps1 -Wait
+Start-Process powershell.exe -Wait -ArgumentList $PSScriptRoot\install-system.ps1
 
 Write-Host "Installing user-specific components..." -ForegroundColor "Yellow"
-Start-Process -FilePath $PSScriptRoot\install-user.ps1 -Wait
+Start-Process powershell.exe -Wait -ArgumentList $PSScriptRoot\install-user.ps1
--- win/net-install.ps1
+++ win/net-install.ps1
@@ -54,7 +54,7 @@ if(Test-Path $installDir) { Remove-Item -Path $installDir -Recurse -Force }
 Unzip-File $zip $tempDir
 
 Push-Location $installDir
-Start-Process -FilePath .\install-all.ps1 -Wait
+Start-Process powershell.exe -Wait -ArgumentList $PSScriptRoot\install-all.ps1
 Pop-Location
 
 Write-Host "Removing installation files..." -ForegroundColor "Yellow"
