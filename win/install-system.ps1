f82eac342057c4fecdd19ead1c79ba36e5da5592
silvarc <silvarc@tutanota.com>
2024-01-15 02:42:15 +0100

    Move winget update earlier to allow uninstallation

--- win/install-system.ps1
+++ win/install-system.ps1
@@ -10,22 +10,6 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 . $PSScriptRoot/setup-task-scheduler.ps1
 . $PSScriptRoot/setup-windows.ps1
 
-Write-Host "Updating Winget..." -ForegroundColor "Yellow"
-$results = (winget --version) | Select-String -Pattern 'v(\d)\.(\d).*'
-$major = $results.Matches.Groups[1].Captures.Value -le 1
-$minor = $results.Matches.Groups[2].Captures.Value -lt 6
-
-if ($major -and $minor) {
-    $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-    $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
-    Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
-    Add-AppxPackage winget.msixbundle
-    Remove-Item winget.msixbundle
-}
-else {
-    winget upgrade winget --silent --accept-package-agreements --accept-source-agreements
-}
-
 Write-Host "Installing packages..." -ForegroundColor "Yellow"
 $packagesList = "$PSScriptRoot\packages-list.json"
 $packagesListObject = Get-Content -Raw -Path $packagesList | ConvertFrom-Json
--- win/setup-windows.ps1
+++ win/setup-windows.ps1
@@ -328,6 +328,23 @@ Set-ItemProperty "HKCU:\System\GameConfigStore" "GameDVR_Enabled" -Type Dword -V
 
 Write-Host "Configuring Default Windows Applications..." -ForegroundColor "Yellow"
 
+# Update Winget
+Write-Host "Updating Winget..." -ForegroundColor "Yellow"
+$results = (winget --version) | Select-String -Pattern 'v(\d)\.(\d).*'
+$major = $results.Matches.Groups[1].Captures.Value -le 1
+$minor = $results.Matches.Groups[2].Captures.Value -lt 6
+
+if ($major -and $minor) {
+    $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
+    Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
+    Add-AppxPackage winget.msixbundle
+    Remove-Item winget.msixbundle
+}
+else {
+    winget upgrade winget --silent --accept-package-agreements --accept-source-agreements
+}
+
 # Uninstall 3D Builder
 Get-AppxPackage "Microsoft.3DBuilder" -AllUsers | Remove-AppxPackage -AllUsers
 Get-AppXProvisionedPackage -Online | Where-Object DisplayName -like "Microsoft.3DBuilder" | Remove-AppxProvisionedPackage -Online -AllUsers
@@ -541,7 +558,10 @@ Get-AppxPackage "Microsoft.OutlookForWindows" -AllUsers | Remove-AppxPackage -Al
 Get-AppXProvisionedPackage -Online | Where-Object DisplayName -like "Microsoft.OutlookForWindows" | Remove-AppxProvisionedPackage -Online -AllUsers
 
 # Uninstall OneDrive
-#winget uninstall Microsoft.OneDrive --force --silent --accept-source-agreements --disable-interactivity
+winget uninstall Microsoft.OneDrive --force --silent --accept-source-agreements --disable-interactivity
+
+# Uninstall Windows Terminal
+winget uninstall Microsoft.WindowsTerminal --force --silent --accept-source-agreements --disable-interactivity
 
 Write-Host "Configuring Accessibility..." -ForegroundColor "Yellow"
 
