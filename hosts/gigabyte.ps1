e85832d2126c56eef6414522cd6a19fceca8ee36
silvarc141 <silvarc141@users.noreply.github.com>
2024-05-15 03:59:02 +0200

    rename

--- hosts/gigabyte.ps1
+++
@@ -1,2 +0,0 @@
-. { (new-object net.webclient).DownloadString("https://raw.github.com/silvarc141/os-setup/main/run-install-script-in-context.ps1") | Invoke-Expression }
-Run-InstallScriptInContext -Account silvarc141 -Repo os-setup -Branch main
